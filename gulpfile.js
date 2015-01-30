c8e40cac187b8029dd80ec8c51eda87febaca8ce
Susan Buck <susan@susanbuck.net>
2015-01-29 22:49:23 -0500

    Organized less files into modules. Added Gulp to run the less processing.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp   = require('gulp');
+var less   = require('gulp-less');
+var path   = require('path');
+var notify = require('gulp-notify');
+var gutil  = require('gulp-util');
+
+/* PARAMS */
+var target = './less/freshlaundry.less';
+
+// Specify what your default tasks are... i.e what should run when you run 'gulp' in CL
+// Our tasks here are 'less' and 'watch'
+gulp.task('default', ['less','watch']);
+
+    // Set up watch task
+    gulp.task('watch', function() {
+
+        // Watch the css folder, and upon updates run the css task
+        gulp.watch('/less/*.less', ['less']);
+    }
+);
+
+/* TASKS */
+gulp.task('less', function () {
+  gulp.src(target)
+    .pipe(less({compress: false}).on('error', gutil.log))
+    .pipe(gulp.dest('css/'))
+    .pipe(notify('Less Compiled'));
+});
+
+
+
