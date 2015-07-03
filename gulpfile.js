3f9465589b148f3182c07e9bd110da83022809b7
Susan Buck <susan@susanbuck.net>
2015-07-03 01:37:06 -0400

    Color tweaking.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,9 @@ notify = require("gulp-notify"),
 changed = require('gulp-changed'),
 debug = require('gulp-debug');
 
-
 gulp.task('less', function () {
 
-  var onError = function(err) {
+    var onError = function(err) {
       notify.onError({
           title:    "Error compiling less file in ",
           subtitle: "<%= error.filename.replace(/^.*[\\\/]/, '') %>",
@@ -18,7 +17,7 @@ gulp.task('less', function () {
       this.emit('end')
     };
 
-  return gulp.src('less/**/*.less')
+    gulp.src('less/**/*.less')
     .pipe(changed( 'css', {extension: '.css'}))
     .pipe(plumber({errorHandler: onError}))
     .pipe(less({
@@ -27,12 +26,21 @@ gulp.task('less', function () {
     .pipe(debug({title: 'Succesfully compiled: '}))
     .pipe(notify("Succesfully compiled <%= file.relative %>!"))
     .pipe(gulp.dest('./css'));
+
+    // Re-compile the main less file
+    gulp.src('./less/freshlaundry.less')
+    .pipe(less({
+      paths: [ './less/' ]
+    })) 
+    .pipe(gulp.dest('./css/'));
+
 });
 
 
 gulp.task('watch', function() {
   gulp.watch("less/**/*.less", ["less"]);
 });
+
 
 gulp.task('default', ['watch']);
 
