d63a04fd910f829811a5945b23b5cb41a62fa40b
Susan Buck <susan@susanbuck.net>
2018-01-11 19:00:39 -0500

    Make labels black

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,15 @@
 let mix = require('laravel-mix');
 
+// After you make changes to this file run `npm run dev` or `nd`
+
 // Compile less to CSS
 // All the less includes are in freshlaundry.less so we only have one file to do here
-mix.less('src/less/freshlaundry.less', 'dist/');
+mix.less('src/less/freshlaundry.less', 'dist/freshlaundry.css');
 
 // Concatenate all .js files into one
 mix.scripts([
     'src/js/common.js',
     'src/js/freshlaundry.js',
     'src/js/retina.js'
-], 'dist/freshlaundry.js').version();
+], 'dist/freshlaundry.js');
 
