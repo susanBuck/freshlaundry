8fa17801964078e76019ec86c884920b81769627
Susan Buck <susan@susanbuck.net>
2018-11-09 16:09:11 -0500

    Table updates

--- demo/index.php
+++ demo/index.php
@@ -334,13 +334,72 @@
     <section>
         <h2>Tables</h2>
 
+        <h3>Default</h3>
         <table>
             <thead>
+                <tr>
+                    <th data-field="id">Name</th>
+                    <th data-field="name">Item Name</th>
+                    <th data-field="price">Item Price</th>
+                </tr>
+            </thead>
+
+            <tbody>
             <tr>
-                <th data-field="id">Name</th>
-                <th data-field="name">Item Name</th>
-                <th data-field="price">Item Price</th>
+                <td>Alvin</td>
+                <td>Eclair</td>
+                <td>$0.87</td>
             </tr>
+            <tr>
+                <td>Alan</td>
+                <td>Jellybean</td>
+                <td>$3.76</td>
+            </tr>
+            <tr>
+                <td>Jonathan</td>
+                <td>Lollipop</td>
+                <td>$7.00</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <h3>class='lined hoverable'</h3>
+        <table class='lined hoverable'>
+            <thead>
+                <tr>
+                    <th data-field="id">Name</th>
+                    <th data-field="name">Item Name</th>
+                    <th data-field="price">Item Price</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>Alvin</td>
+                <td>Eclair</td>
+                <td>$0.87</td>
+            </tr>
+            <tr>
+                <td>Alan</td>
+                <td>Jellybean</td>
+                <td>$3.76</td>
+            </tr>
+            <tr>
+                <td>Jonathan</td>
+                <td>Lollipop</td>
+                <td>$7.00</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <h3>class='striped centered'</h3>
+        <table class='striped centered'>
+            <thead>
+                <tr>
+                    <th data-field="id">Name</th>
+                    <th data-field="name">Item Name</th>
+                    <th data-field="price">Item Price</th>
+                </tr>
             </thead>
 
             <tbody>
